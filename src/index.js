7696024dfca99e806dd7017224b224739dc7e202
orest-forest <danilchenko.orest@gmail.com>
2020-10-25 14:54:20 +0200

    feat: add new functions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let config = {};
+
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        config[bracketsConfig[i][0]] = bracketsConfig[i][1]
+    }
+
+
+    let stack = [];
+    let ret = '';
+
+    for (let i = 0; i < str.length; i++) {
+        let c = str[i];
+
+        if (config[c]) {
+            stack.push(c);
+        } else {
+            if (stack.length === 0) {
+                return false;
+            }
+
+            let bracket = stack.pop();
+
+            c = config[bracket];
+        }
+
+        ret += c;
+    }
+    if (stack.length > 0) {
+        return false;
+    }
+    return true
 }
